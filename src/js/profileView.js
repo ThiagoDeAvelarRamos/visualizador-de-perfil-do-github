62fe1480d96b593a26f80ad15c43703bc797a84b
Thiago-Avelar <thiago.de.avelar.ramos@gmail.com>
2026-08-20 20:02:55 -0300

    Adiciona lista com os 10 ultimos repositorios

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -1,9 +1,21 @@
-export function renderProfile(userData, container) {
+export function renderProfile(userData, userRepos, container) {
+
+  const reposHtml = userRepos.length > 0 ? userRepos.map(repo => `
+    <div class="repository-card">
+      <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
+      <div class ="repository-stats">
+      <span>⭐ Stars: ${repo.stargazers_count}</span>
+      <span>🍴 Forks: ${repo.forks_count}</span>
+      <span>👀 Watchers: ${repo.watchers_count}</span>
+      <span>📝 language: ${repo.language || "Não especificada"}</span>
+      </div>
+    </div>
+    `).join('') : `<p class="no-repos">Este usuário não possui repositórios públicos 😢.</p>`;
+
   container.innerHTML = `
     <div class="profile-card">
-      <img src="${userData.avatar_url}" alt="Avatar de ${
-    userData.name
-  }" class="profile-avatar">
+      <img src="${userData.avatar_url}" alt="Avatar de ${userData.name
+    }" class="profile-avatar">
       <div class="profile-info">
         <h2>${userData.name}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
@@ -18,6 +30,13 @@ export function renderProfile(userData, container) {
         <div class="following">
             <h4>👥 Seguindo</h4>
             <span>${userData.following}</span>
+        </div>
+    </div>
+
+    <div class="profile-repositories">
+        <h3>Repositórios</h3>
+      <div class="repositories">
+        ${reposHtml}
         </div>
     </div>
   `;
